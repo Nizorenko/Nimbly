e203b2159d753065ba745be695d48dc74953b176
Nizorenko <91205091+Nizorenko@users.noreply.github.com>
2021-12-08 16:28:04 +0500

    Added tabs
    Added tabs so far without animation

---
+++ js/tabsAppPreview.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// Таб appPreview
+
+let tab = function () {
+  // Создаём переменные с колекцией навигации и колекцией карточек,
+  // так же создаём переменную для переключения навигации и контента.
+
+  let tabNav = document.querySelectorAll(".js-tab__navBtn"),
+    tabContent = document.querySelectorAll(".js-tab__contentItem"),
+    tabName;
+
+  //Добавляем слушатель "click" на каждый элемент в коллекции
+
+  tabNav.forEach((item) => {
+    item.addEventListener("click", selectTabNav);
+  });
+
+  function selectTabNav() {
+    // Убираем класс css-tab__active у всех элементов
+
+    tabNav.forEach((item) => {
+      item.classList.remove("css-tab__active");
+    });
+
+    // Добовляем класс css-tab__active у элемента на котороый нажали this
+
+    this.classList.add("css-tab__active");
+
+    // Добовляем в переменную tabName номер атрибута по которому кликнули
+
+    tabName = this.getAttribute("data__number");
+    selectTabContent(tabName);
+  }
+
+  function selectTabContent(tabName) {
+    tabContent.forEach((item) => {
+      if (item.classList.contains(tabName)) {
+        item.classList.add("css-tab__active");
+      } else {
+        item.classList.remove("css-tab__active");
+      }
+      //  item.classList.contains(tabName)
+      // ? item.classList.add("css-tab__active")
+      // : item.classList.remove("css-tab__active");
+    });
+  }
+};
+
+tab();
